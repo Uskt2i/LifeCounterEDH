a0847adb7d6857729439a64a6632f22a8af59f38
Uskt2i <yusuketanicg@gmail.com>
2024-08-25 18:31:19 +0900

    Resize button

--- app/src/main/java/com/example/lifecounteredh/MainActivity.kt
+++ app/src/main/java/com/example/lifecounteredh/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // フルスクリーンモードを設定
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
